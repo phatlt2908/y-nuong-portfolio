723779b85cec641231054f490503ca5fec908925
phatlt <phatlt@jienielab.asia>
2024-02-22 23:21:29 +0700

    add sidebar and font

---
+++ src/app/fonts.js
@@ -0,0 +1,15 @@
+import { Just_Another_Hand, Just_Me_Again_Down_Here } from "next/font/google";
+
+export const just_another_hand = Just_Another_Hand({
+  variable: '--font-just-another-hand',
+  subsets: ["latin"],
+  display: "swap",
+  weight: '400',
+});
+
+export const just_another_hand_down_here = Just_Me_Again_Down_Here({
+  variable: '--font-just-another-hand-down-here',
+  subsets: ["latin"],
+  display: "swap",
+  weight: '400',
+});
